235b3cac048857cde5208bec069b22164ccc3b5a
Anderson <anderson1908x@gmail.com>
2026-04-14 09:33:45 -0500

    reinstalando el proyecto

---
+++ nuevo_proyecto/script.js
@@ -0,0 +1,8 @@
+// 1. Buscamos el botón por su ID
+const boton = document.getElementById('miBoton');
+
+// 2. Escuchamos el evento 'click'
+boton.addEventListener('click', () => {
+    alert('¡Hola! Has activado el God Mode 🚀');
+    console.log('El usuario hizo clic en el botón');
+});
